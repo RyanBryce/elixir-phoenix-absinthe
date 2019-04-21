360ac56617aae132b6bfff78c5122e84a872f037
Ryan Bryce <ryan.bryce1992@gmail.com>
2019-04-20 22:39:15 -0600

    added more methods to the Cards module added {:ex_doc, ~

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,9 +1,15 @@
 defmodule Cards do
+  @moduledoc """
+    provides methods for createing and handleing deck of cards
+  """
+  @doc """
+    returns a list of string representing a dec of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", ];
     suits = ["Spades", "Clubs", "Hearts", "Dimonds"]
     # list comprehension is technically a JS .map
-    cards = for suit <- suits, value <- values do
+    for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
  
@@ -12,7 +18,42 @@ defmodule Cards do
   def shuffle(deck) do
     Enum.shuffle deck
   end
+
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  @doc """
+    Divides a deck into hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in a hand
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.create.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+  
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "that file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+
   end
 end
