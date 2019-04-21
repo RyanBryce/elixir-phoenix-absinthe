ddc45c121af9d89e39046e9566fd4771f67c2724
Ryan Bryce <ryan.bryce1992@gmail.com>
2019-04-20 20:41:33 -0600

    created deck of cards using elixir list comprehensions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,9 +1,18 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", ];
+    suits = ["Spades", "Clubs", "Hearts", "Dimonds"]
+    # list comprehension is technically a JS .map
+    cards = for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+ 
   end
 
   def shuffle(deck) do
     Enum.shuffle deck
+  end
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
