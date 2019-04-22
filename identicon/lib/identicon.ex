9f04d6a317b97cf5c03c40b1130820facecee29f
Ryan Bryce <ryan.bryce1992@gmail.com>
2019-04-21 21:17:31 -0600

    created migration for topics table and create table by runnnig mix ecto.migrate

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -38,6 +38,7 @@ defmodule Identicon do
 
     %Identicon.Image{image | pixel_map: pixel_map}
   end
+
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
     grid = Enum.filter grid, fn({code, _index}) ->
       rem(code, 2) == 0
